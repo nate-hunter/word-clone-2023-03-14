0f56fa36e87a73112bfe7e46b9a2b249144ac016
Nate Hunter <nhunter@indemand.com>
2023-03-16 09:57:38 -0400

    added validation (using the `pattern` attr) to the guess `input`

--- src/components/GuessInput/GuessInput.js
+++ src/components/GuessInput/GuessInput.js
@@ -4,7 +4,14 @@ const GuessInput = ({ guess, handleChange, handleSubmit }) => {
   return (
     <form className='guess-input-wrapper' onSubmit={handleSubmit}>
       <label htmlFor='guess-input'>Enter guess:</label>
-      <input type='text' id='guess-input' name='guess' value={guess} onChange={handleChange} />
+      <input
+        type='text'
+        id='guess-input'
+        name='guess'
+        value={guess}
+        onChange={handleChange}
+        pattern={'[a-zA-Z]{5}'}
+      />
     </form>
   );
 };
