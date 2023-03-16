efa698caa5af879b1bdeaace6e60cb4a29d6afbc
Nate Hunter <nhunter@indemand.com>
2023-03-16 10:25:11 -0400

    rendered guesses in `GuessList` + set up error message for duplicate guesses

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -15,6 +15,8 @@ console.info({ answer });
 // Also, using as a way to track files I touch - maybe helpful, maybe not
 const Game = () => {
   const [guess, setGuess] = useState('');
+  const [guesses, setGuesses] = useState([]);
+  const [errorMsg, setErrorMsg] = useState('');
 
   const handleGuessChange = (e) => {
     setGuess(e.target.value.toUpperCase());
@@ -23,14 +25,24 @@ const Game = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    console.log({ guess });
+    if (guesses.includes(guess)) {
+      setErrorMsg(`Try a new word. '${guess}' has already been guessed.`);
+      setGuess('');
+      return;
+    }
+
+    const updatedGuesses = [...guesses];
+    updatedGuesses.push(guess);
+    setGuesses(updatedGuesses);
+
     setGuess('');
   };
 
   return (
     <>
-      <GuessList />
+      <GuessList guesses={guesses} />
       <GuessInput guess={guess} handleChange={handleGuessChange} handleSubmit={handleSubmit} />
+      <h4 style={{ color: 'red' }}>{errorMsg}</h4>
     </>
   );
 };
--- src/components/GuessList/GuessList.js
+++ src/components/GuessList/GuessList.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
-const GuessList = () => {
+const GuessList = ({ guesses }) => {
+  console.log('[guesses]', guesses);
+
   return (
     <>
       <h3>LIST OF GUESSES...</h3>
-      <div className='guess-result'></div>
+      <div className='guess-result'>{guesses}</div>
     </>
   );
 };
