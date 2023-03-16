d4ac18eee36d241ae29c57c322efb5c9e9b936e2
Nate Hunter <nhunter@indemand.com>
2023-03-16 11:11:14 -0400

    imported `range()` + constant to `GuessResult` to render (6) `Guess` components

--- src/components/GuessResults/GuessResults.js
+++ src/components/GuessResults/GuessResults.js
@@ -1,12 +1,16 @@
 import React from 'react';
+
+import { NUM_OF_GUESSES_ALLOWED } from '../../constants';
+import { range } from '../../utils';
+
 import Guess from '../Guess';
 
 const GuessResults = ({ guesses }) => {
   return (
     <div className='guess-results'>
-      {guesses.map((guess) => (
+      {range(NUM_OF_GUESSES_ALLOWED).map((guess) => (
         <p key={guess} className='guess'>
-          {guess}
+          <Guess />
         </p>
       ))}
     </div>
