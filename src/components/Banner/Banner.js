302f7269844882ecd7a632d991650acf9d426e60
Nate Hunter <nhunter@indemand.com>
2023-03-22 10:37:18 -0400

    updated `Banner` with game props

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,18 +1,21 @@
 import React from 'react';
 
-function Banner({ gameResult }) {
-  const bannerType = gameResult === 'win' ? 'happy' : 'sad';
-  const text =
-    gameResult === 'win' ? (
-      <p>
-        <strong>Congratulations!</strong> Got it in
-        <strong>3 guesses</strong>.
-      </p>
-    ) : (
-      <p>
-        Sorry, the correct answer is <strong>LEARN</strong>.
-      </p>
-    );
+function Banner({ isGameWon, answer, numOfGuesses }) {
+  const bannerType = isGameWon ? 'happy' : 'sad';
+  const guessText = numOfGuesses === 1 ? `guess` : `guesses`;
+  const text = isGameWon ? (
+    <p>
+      <strong>Congratulations!</strong> Got it in{' '}
+      <strong>
+        {numOfGuesses} {guessText}
+      </strong>
+      .
+    </p>
+  ) : (
+    <p>
+      Sorry, the correct answer is <strong>{answer}</strong>.
+    </p>
+  );
 
   return <div className={`${bannerType} banner`}>{text}</div>;
 }
